d2395ba4372733b90d4c0c07497505a81725e182
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-01 00:46:42 +0600

    init

---
+++ client/doc/Project Notes.js
@@ -0,0 +1,12 @@
+//=> Step-1:
+//=> Install Vite: 'npm create vite@latest -- --template react client'
+
+//=> Step-2:
+//=> install package:
+//-->> three
+//-->> @react-three/fiber
+//-->> @react-three/drei
+//-->> maath
+//-->> valtio
+//-->> react-color
+//-->> framer-motion
