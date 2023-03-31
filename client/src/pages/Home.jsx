15e23a01c42516bb8ce25cc850533c09bc6e415b
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-01 02:20:30 +0600

    project setup

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+import {
+	headContainerAnimation,
+	headContentAnimation,
+	headTextAnimation,
+	slideAnimation,
+} from '../config/motion';
+
+import state from '../store';
 
 const Home = () => {
-	return <div>Home</div>;
+	const snap = useSnapshot(state);
+
+	return <AnimatePresence></AnimatePresence>;
 };
 
 export default Home;
