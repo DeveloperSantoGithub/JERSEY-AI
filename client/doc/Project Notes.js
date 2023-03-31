e1d9d0986227f6fd9acec597a1c40e7aaea1c76f
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-01 00:48:50 +0600

    project setup

--- client/doc/Project Notes.js
+++ client/doc/Project Notes.js
@@ -1,5 +1,6 @@
 //=> Step-1:
-//=> Install Vite: 'npm create vite@latest -- --template react client'
+//=> Install Vite:
+//-->> npm create vite@latest -- --template react client
 
 //=> Step-2:
 //=> install package:
