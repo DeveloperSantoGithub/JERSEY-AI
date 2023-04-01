31096f2376696cee67f3c612f424a68f57077a3d
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-02 00:49:27 +0600

    Adding functionality on Tabs

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+import state from '../store';
 
 const ColorPicker = () => {
-	return <div>ColorPicker</div>;
+	const snap = useSnapshot(state);
+
+	return (
+		<div className="absolute left-full ml-3">
+			<SketchPicker
+				color={snap.shirtColor}
+				disableAlpha
+				onChange={(color) => (state.shirtColor = color.hex)}
+				presetColors={[
+					'#ccc',
+					'#4F5E8C',
+					'#F2762E',
+					'#5741A6',
+					'#D9A273',
+					'#213A40',
+					'#EAF205',
+					'#F25764',
+					'#734124',
+					'#262626',
+					'#F2622E',
+					'#BBF2E8',
+				]}
+			/>
+		</div>
+	);
 };
 
 export default ColorPicker;
