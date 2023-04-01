9566fb2b6983fc548350bcf2156a1368b8c96ef6
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-01 13:52:26 +0600

    Canvas

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -13,7 +13,14 @@ const CameraRig = ({ children }) => {
 		const isMobile = window.innerWidth <= 600;
 
 		//=> Set Initial Position of the Model:
-		let targetPosition = [];
+		let targetPosition = [-0.4, 0, 2];
+		if (snap.intro) {
+			if (isBreakpoint) targetPosition = [0, 0, 2];
+			if (isMobile) targetPosition = [0, 0.2, 2.5];
+		} else {
+			if (isMobile) targetPosition = [0, 0, 2.5];
+			else targetPosition = [0, 0, 2];
+		}
 
 		//=> Set Model Rotation Smoother:
 		easing.dampE(
