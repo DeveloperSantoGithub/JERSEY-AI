56cb0067328b1456f19bcee5935ca87ba751c1e7
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-02 02:40:46 +0600

    Adding functionality on Tabs

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import CustomButton from './CustomButton';
 
 const FilePicker = ({ file, setFile, readFile }) => {
-	return <div className="filepicker-container"></div>;
+	return (
+		<div className="filepicker-container">
+			<input
+				id="file-upload"
+				type="file"
+				accept="image/*"
+				onChange={(e) => setFile(e.target.files[0])}
+			/>
+
+			<label htmlFor="file-upload" className="filepicker-label">
+				Upload File
+			</label>
+		</div>
+	);
 };
 
 export default FilePicker;
