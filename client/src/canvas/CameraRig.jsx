a79c42bc32b68e5d466e381ac814eaf652683cc4
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-01 13:28:05 +0600

    Canvas

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -10,6 +10,15 @@ const CameraRig = ({ children }) => {
 
 	//=> Set Model Rotation Smoother:
 
+	useFrame((state, delta) => {
+		easing.dampE(
+			group.current.rotation,
+			[state.pointer.y / 10, -state.pointer.x / 5, 0],
+			0.25,
+			delta,
+		);
+	});
+
 	return <group ref={group}>{children}</group>;
 };
 
