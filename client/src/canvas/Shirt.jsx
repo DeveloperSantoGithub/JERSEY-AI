7b942f1f7f3005dcc6c1a207e61337057732f39c
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-01 12:26:17 +0600

    Canvas

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
+
+import state from '../store';
 
 const Shirt = () => {
+	const snap = useSnapshot(state);
+
+	//=> Import Model:
+
 	return <div>Shirt</div>;
 };
 
