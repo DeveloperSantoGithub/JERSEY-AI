6b6f68d343d5ab2c79d8db6fae72bc0a6a63862e
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-01 12:03:00 +0600

    Canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,6 +1,8 @@
 import { Canvas } from '@react-three/fiber';
 import { Environment, Center } from '@react-three/drei';
 
+import Backdrop from './Backdrop';
+
 const CanvasModel = () => {
 	return <div>CanvasModel</div>;
 };
