e7106c6fa23d88c950bd74200eb9d035d2fb9fd2
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-02 12:00:17 +0600

    add server

--- client/doc/Project Notes.js
+++ client/doc/Project Notes.js
@@ -1,8 +1,10 @@
-//=> Step-1:
+//---------------------- Client Part ----------------------//
+
+//=> Step-0:
 //=> Install Vite:
 //-->> npm create vite@latest -- --template react client
 
-//=> Step-2:
+//=> Step-1:
 //=> install package:
 //-->> three : 3D threeJS lib
 //-->> @react-three/fiber : React render for threeJS lib
@@ -13,7 +15,7 @@
 //-->> framer-motion : cool animation
 //-->> tailwindcss : css styling
 
-//=> Step-3:
+//=> Step-2:
 //=> Adding Files:
 //-->> Public
 //-->> Assets
@@ -21,5 +23,25 @@
 //-->> Config
 // #FFB30D
 
+//--------------------------- Server Part -----------------------//
 //==>> Server Setup:
-//-->> npm init -y
+
+//=> Step-0:
+//-->> Create : Server diretory
+//-->> cd to : Server
+//-->> run in the terminal: npm init -y
+
+//=> Step-1:
+//>-->> Inside Package.json:
+//-->> Add to 'Scripts' : { 'start': "nodemon index" } -> it keeps the index file running, so it look into all the changes.
+
+//-->> Add to 'top {}' : "type" : "module" -> it going to allow us to use modern 'import, export' system
+
+//=> install packages:
+//-->> cloudinary : Powerful Image and Video APIs
+//-->> cors : To allow for cros origin requests
+//-->> dotenv : for env file
+//-->> express
+//-->> mongoose : Node. js-based Object Data Modeling (ODM) library for MongoDB
+//-->> nodemon :  It monitors your project directory and automatically restarts your node application when it detects any changes.
+//-->> openai
