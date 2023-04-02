fe6754fb68147ae8272c73891620b149ef4ada81
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-02 13:31:07 +0600

    server setup

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -5,11 +5,39 @@ import { Configuration, OpenAIApi } from 'openai';
 //=> Environment Variable:
 dotenv.config();
 
-//=> Route:
+//=> Route config:
 const router = express.Router();
 
+//=> Dalle Api:
+const config = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+//=> Requests:
 router.route('/').get((req, res) => {
 	res.status(200).json({ message: 'This it from route: /' });
+});
+
+router.route('/').post(async (req, res) => {
+	try {
+		const { prompt } = req.body;
+
+		const response = await openai.createImage({
+			prompt,
+			n: 1,
+			size: '1024x1024',
+			response_format: 'b64_json',
+		});
+
+		const image = response.data.data[0].b64_json;
+
+		res.status(200).json({ photo: image });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Something went wrong' });
+	}
 });
 
 export default router;
